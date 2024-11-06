5cfcb0e64a0f714b07b57c6776406750e6560b84
Jason Walsh <j@wal.sh>
2024-11-06 16:53:04 -0500

    feat(eval): add evaluation.quoth namespace with syntax quote tests
    Added src/evaluation/quoth.clj file containing namespace, raven
    variable, and syntax-quote-tests function.

---
+++ src/evaluation/quoth.clj
@@ -0,0 +1,12 @@
+(ns evaluation.quoth)
+
+(def raven "nevermore")
+
+(defn syntax-quote-tests
+  []
+  [
+   (= raven "nevermore")
+   (= 'raven (symbol "raven"))
+   (not= 'raven (symbol "user/raven"))
+   (= '(a b c) '(a b c))
+   ])
